5d43467cd5a449e48efcb827dfb5f519f91bc6bd
KrisHeathNREL <kheath@nrel.gov>
2025-01-29 08:59:24 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 import os
-version_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'openspecy', '__version__.py')
+version_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'openspecy_python_interface', '_version.py')
 with open(version_file) as f:
     exec(f.read())
 
@@ -32,5 +32,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'bizstyle'
+html_theme_options = {'sidebarwidth': 200 }
 html_static_path = ['_static']
